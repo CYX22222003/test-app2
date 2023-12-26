4867b2354f091c0351a82b18870ef1272628a7a9
CYX22222003 <e1155533@u.nus.edu>
2023-12-26 23:35:00 +0800

    revert attempt to revert file

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import './App.css';
 
 function App() {
   return (
+<<<<<<< HEAD
     <div className="App">
       <h1 className='App-header'>First Deployment on Github Pages</h1>
       <p className='App-Content'>This is my first webpage deployed on Github pages. More content may be added in the future!</p>
     </div>
+=======
+    <Router>
+      <div className='App'>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/Intro" element={<Intro />} />
+        </Routes>
+      </div>
+    </Router>
+>>>>>>> parent of ea7aab5 (adding dependency for routing)
   );
 }
 
