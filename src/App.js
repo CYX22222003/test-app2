5175f8955893cc0990f096e74aa5db7ddca0b044
CYX22222003 <e1155533@u.nus.edu>
2023-12-26 23:32:21 +0800

    Revert "add new link"
    This reverts commit 913aea54767524cd133ecb602d78c3fc34e44dee.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Home';
-import Intro from './Intro';
 
 function App() {
   return (
     <div className="App">
       <h1 className='App-header'>First Deployment on Github Pages</h1>
-      <p>This is my first webpage deployed on Github pages. More content may be added in the future!</p>
+      <p className='App-Content'>This is my first webpage deployed on Github pages. More content may be added in the future!</p>
     </div>
   );
 }
