bdf88c0837c91c89539f6b0e887f8a67c0866527
CYX22222003 <e1155533@u.nus.edu>
2023-12-26 22:17:56 +0800

    add new format

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1 className='App-header'>First Deployment on Github Pages</h1>
-      <p>This is my first webpage deployed on Github pages. More content may be added in the future!</p>
+      <p className='App-Content'>This is my first webpage deployed on Github pages. More content may be added in the future!</p>
     </div>
   );
 }
