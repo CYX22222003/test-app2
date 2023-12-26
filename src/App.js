6a4587e64adf859255edba73c146c5e6d5732a31
CYX22222003 <e1155533@u.nus.edu>
2023-12-26 21:57:12 +0800

    update css and static webpage

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>test app</p>
+      <h1 className='App-header'>First Deployment on Github Pages</h1>
+      <p>This is my first webpage deployed on Github pages. More content may be added in the future!</p>
     </div>
   );
 }
