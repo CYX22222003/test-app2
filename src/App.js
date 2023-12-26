ea7aab5f6a891ab5af0b6131350e98d883fc7f88
CYX22222003 <e1155533@u.nus.edu>
2023-12-26 23:12:34 +0800

    adding dependency for routing

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <div className='App'>
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route exact path="/Intro" element={<Intro />} />
         </Routes>
       </div>
