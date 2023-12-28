813d9c33c3bcd4d98b29f4c609e447777065ebda
CYX22222003 <e1155533@u.nus.edu>
2023-12-28 11:11:57 +0800

    add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-import Home
- from './Home';
+import Home from './Home';
+import About from './About';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </Router>
   );
 }
 
