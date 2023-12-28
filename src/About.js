7bfbc42821133fc1985c1ed28d0ea7461690c069
CYX22222003 <e1155533@u.nus.edu>
2023-12-28 11:13:35 +0800

    correct bugs in the routing configuration

--- src/About.js
+++ src/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css"
 
-export default function Home(){
+export default function About(){
     return (
     <div className="About">
         <h1>About us</h1>
