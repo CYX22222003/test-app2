0ab94b013262c2ae92b1535a60a50d70eacd0315
CYX22222003 <e1155533@u.nus.edu>
2023-12-28 11:28:40 +0800

    switch to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import Home from './Home';
 import About from './About';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import {Routes, Route } from 'react-router-dom';
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
     </Routes>
-  </Router>
+    </>
   );
 }
 
