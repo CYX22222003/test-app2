492a8a4c96aa6b92b0c48fd262b6ecf66efdb1a6
CYX22222003 <e1155533@u.nus.edu>
2023-12-28 11:17:01 +0800

    correct bugs in the routing configuration

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from './About';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
